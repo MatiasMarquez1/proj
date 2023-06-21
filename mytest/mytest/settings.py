5e30f38c47387bc20b463bd253916754f8a44bec
MatiasMarquez1 <matias.marquez.14@sansano.usm.cl>
2023-06-21 21:15:42 +0000

    Moving the project to my account

--- mytest/mytest/settings.py
+++ mytest/mytest/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -57,7 +56,7 @@ ROOT_URLCONF = 'mytest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],   # This line tells django where the templates are located
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mytest/polls/admin.py
+++ mytest/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# This makes it so that you can edit Questions from the admin page.
+from .models import Question
+
+admin.site.register(Question)
+
