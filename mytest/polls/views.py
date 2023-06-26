f1bfc65ee0faba67b7c4add1f4c83317f6b0ca61
MatiasMarquez1 <45049217+MatiasMarquez1@users.noreply.github.com>
2023-06-26 19:55:22 -0400

    Update views.py

--- mytest/polls/views.py
+++ mytest/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 
+from .models import Question
+
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
