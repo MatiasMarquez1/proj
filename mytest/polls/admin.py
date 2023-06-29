e678520415a19f231c7eb30b358e1369038b99f8
MatiasMarquez1 <45049217+MatiasMarquez1@users.noreply.github.com>
2023-06-29 00:40:34 -0400

    Update admin.py
    Registering Choice

--- mytest/polls/admin.py
+++ mytest/polls/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 # This makes it so that you can edit Questions from the admin page.
-from .models import Question
+from .models import Question, Choice
 
 admin.site.register(Question)
+admin.site.register(Choice)
 
