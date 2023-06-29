408c3938bb72a5f8361e12ebaf936f896e00206f
MatiasMarquez1 <45049217+MatiasMarquez1@users.noreply.github.com>
2023-06-28 20:39:15 -0400

    Update models.py
    Added comments

--- mytest/polls/models.py
+++ mytest/polls/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 # Modelo de preguntas, con texto y fecha
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    question_text = models.CharField(max_length=200)    # Texto de la pregunta
+    pub_date = models.DateTimeField("date published")   # Fecha de publicación
 
     def __str__(self):
         return self.question_text
 
 # Modelo de elección, con pregunta, texto y votos
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)    # Nombre de la pregunta
+    choice_text = models.CharField(max_length=200)                      # Nombre de la elección
+    votes = models.IntegerField(default=0)                              # Cantidad de votos
 
     def __str__(self):
         return self.choice_text
