5f157451826bc2679f843e863668914f947864dc
Mati <denderegue@gmail.com>
2023-06-30 21:18:02 +0000

    Updating poll/admin.py

--- mytest/polls/admin.py
+++ mytest/polls/admin.py
@@ -5,6 +5,11 @@ from django.contrib import admin
 # This makes it so that you can edit Questions from the admin page.
 from .models import Question, Choice
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+
 admin.site.register(Question)
 admin.site.register(Choice)
+
+admin.site.register(Question, QuestionAdmin)
 
