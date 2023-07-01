7436ac73f81109659193aa6a95995c50676b8747
MatiasMarquez1 <45049217+MatiasMarquez1@users.noreply.github.com>
2023-07-01 01:06:59 -0400

    Update admin.py

--- mytest/polls/admin.py
+++ mytest/polls/admin.py
@@ -5,15 +5,17 @@ from django.contrib import admin
 # This makes it so that you can edit Questions from the admin page.
 from .models import Question, Choice
 
+# This makes it so that you can create choices inside the Questions admin page
+class ChoiceInLine(admin.StackedInLine):
+    model = Choice
+    extra = 3
+    
 class QuestionAdmin(admin.ModelAdmin):
     # The next line divides the fields into fieldsets with a specified title.
     fieldsets = [
         (None, {"fields":["question_text"]}),
         ("Date information", {"fields": ["pub_date"]})
     ]
-
-#admin.site.register(Question)
-admin.site.register(Choice)
 
 admin.site.register(Question, QuestionAdmin)
 
