df8eec8854de1337dc4803c5d2b952e73b981b8a
MatiasMarquez1 <45049217+MatiasMarquez1@users.noreply.github.com>
2023-07-01 16:48:52 -0400

    Update create_user.py

--- mytest/scripts/create_user.py
+++ mytest/scripts/create_user.py
@@ -1,6 +1,3 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mytest.settings")
-
 from django.contrib.auth.models import User
 
 # When creating a User, use the function create_user(username, email=None, password=None, **extra_fields)
